f62daa2549b46f58f2a1a90b8ac1b14ccf79039b
LHAV <lhav@MacBook-Pro-LHAV.local>
2019-04-29 16:34:00 +0300

    Какие то правки

--- bash/delete_old_files.sh
+++ bash/delete_old_files.sh
@@ -1,10 +1,12 @@
 #! /bin/bash
 
 # Скрипт положить, например, в файл /etc/cron.d/delete_confluence_backup.sh
-# Выполнять задание каждый день в 01:00.
+# Добавить в /etc/crontab задание, выполнять каждый день в 01:00.
+
+# Удалять резервные копии Confluence
 # 0 1 * * *       root    /etc/cron.d/delete_confluence_backup.sh >/dev/null 2>&1
 
 #Удалять файлы старше
-days=30
+days=14
 
 find /mnt/vol-01/appdata/confluence/backups -type f -mtime +$days -exec rm -rf {} \;
